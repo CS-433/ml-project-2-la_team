edb0989e24a4f24ca6e1b2b12294e5fefded99ed
Monnet Joris <joris.monnet@epfl.ch>
2022-12-21 09:30:29 +0100

    recenter image in main

--- website/Main.py
+++ website/Main.py
@@ -4,11 +4,11 @@ from helper_website import hide_streamlit, getImage
 st.set_page_config(page_title="Bias Slayer", page_icon=":computer:", layout="wide")
 st.sidebar.image(getImage("logo_cropped"))
 # ---- MAINPAGE ----
-c1, c2, c3 = st.columns([3, 6, 3])
+c1, c2, c3 = st.columns([3, 3, 3])
 with c1:
     st.title("Bias slayer")
 with c2:
-    st.image(getImage("logo"),width=250)
+    st.image(getImage("logo"),width=300)
 with c3:
     st.subheader("This tutorial will help you learn how to spot and stop biased AI4health applications")
 st.markdown("""---""")
